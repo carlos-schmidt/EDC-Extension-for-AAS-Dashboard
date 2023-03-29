1a256e79b4ff5cdeeb14857087d409d317b58ad0
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2023-03-29 13:18:52 +0200

    Fix some previous merge errors

--- src/modules/edc4aas/edc4aas.module.ts
+++ src/modules/edc4aas/edc4aas.module.ts
@@ -25,7 +25,6 @@ import { OwnElementCollectionTemplate } from './components/own-self-description-
 import { SelfDescriptionBrowserComponent } from './components/self-description-browser/self-description-browser.component';
 import { ElementCollectionTemplate } from './components/self-description-browser/template/element-collection-template/element-collection-template.component';
 import { InfoViewTemplate } from './components/template/info-view-template/info-view-template.component';
-import { SelfDescriptionBrowserComponent } from './components/self-description-browser/self-description-browser.component';
 import { ConfigPageComponent } from './components/config-page/config-page.component';
 
 @NgModule({
